2d9eb9ff490bec65111a24e1f3c8e3c615cd2acf
red001-sys <redstarstore0@gmail.com>
2026-03-19 20:02:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,25 @@ import path from "path";
 export default defineConfig(({ mode }) => ({
   base: "./",
 
+  build: {
+    outDir: "dist",
+    sourcemap: mode === "development",
+    minify: "terser",
+    target: "ES2020",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+          ],
+          capacitor: ["@capacitor/core", "@capacitor/android"],
+        },
+      },
+    },
+  },
+
   server: {
     host: "::",
     port: 8080,
